d01f309b793d4d15797c88d7b018cd1d71023992
Jeremypan <phyjeremy@outlook.com>
2024-12-13 17:34:15 +1100

    add datetime validator unittest

--- src/test/groovy/com/kraken/api/web/rest/v1/validator/DateTimeValidatorTest.groovy
+++ src/test/groovy/com/kraken/api/web/rest/v1/validator/DateTimeValidatorTest.groovy
@@ -1,5 +1,7 @@
 package com.kraken.api.web.rest.v1.validator
 
+import com.kraken.api.web.rest.v1.validator.annotations.ValidDateTime
+import jakarta.validation.ConstraintValidatorContext
 import spock.lang.Specification
 import spock.lang.Subject
 
@@ -12,4 +14,25 @@ class DateTimeValidatorTest extends Specification {
         dateTimeValidator = new DateTimeValidator();
     }
 
+    def 'should valid value with #scenario'() {
+        given: "mock params for isValid method"
+        ConstraintValidatorContext context = Mock()
+        ValidDateTime validDateTime = Stub(ValidDateTime.class)
+        validDateTime.pattern() >> "yyyy-MM-dd HH:mm:ss.SSS"
+
+        when: "call isValid method"
+        dateTimeValidator.initialize(validDateTime)
+        def res = dateTimeValidator.isValid(dateTimeInput, context)
+
+        then: "expect output from isValid"
+        noExceptionThrown()
+        res == output
+
+        where: "scenario for dateTimeInput"
+        scenario                | dateTimeInput             || output
+        "valid dateTimeInput"   | "2005-12-31 00:00:00.000" || true
+        "null"                  | null                      || true
+        "empty string"          | " "                       || true
+        "Invalid dateTimeInput" | "01-01-2025"              || false
+    }
 }
